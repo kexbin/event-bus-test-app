461a978ae29162cfd8cf77644db2ef92042285ed
Brian King <briand.king@adhocteam.us>
2024-01-10 14:58:37 -0800

    initial add

---
+++ src/main/java/gov/va/eventbus/test/Main.java
@@ -0,0 +1,9 @@
+package gov.va.eventbus.test;
+
+import static java.lang.System.out;
+
+public class Main {
+  public static void main(String[] args) {
+    out.println("Hello, world!");
+  }
+}
---
+++ src/test/java/gov/va/eventbus/test/MainTest.java
@@ -0,0 +1,8 @@
+package gov.va.eventbus.test;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+  @Test
+  public void testAccept() {}
+}
